1cd7fa978d6953c36c7767f6c9fa80284f311055
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-08-09 16:31:43 -0300

    Configurando as requisições

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>Movies Lib</h2>
-
-    <Outlet />
+      <Outlet />
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,37 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+
 const Home = () => {
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRateMovies = async (url) => {
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect(() => {
+
+        const topRateUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        getTopRateMovies(topRateUrl)
+
+    }, [])
+
   return (
-    <div>Home</div>
+    <div className="container">
+        <h2 className="title">Melhores filmes:</h2>
+        <div className="movies-container">
+            {topMovies.length === 0 && <p>Carregando...</p>}
+            {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        </div>
+    </div>
   )
 }
 
